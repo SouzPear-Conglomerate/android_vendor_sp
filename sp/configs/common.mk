ad3432fada53b4d8f6d7efcf17a7e5d672261498
brandon <bpear96@gmail.com>
2014-11-16 13:33:34 -0800

    Use lollipop bootanimation.

---
+++ sp/configs/common.mk
@@ -0,0 +1,6 @@
+# Local path for prebuilts
+LOCAL_PATH:= vendor/sp/prebuilts/common/system
+
+# Bootanimation support
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/bootanimation.zip:system/media/bootanimation.zip
